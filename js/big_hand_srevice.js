92749a510f7386d9c449639d3fcda0fc66c6d99c
eojing <105007348+eojing@users.noreply.github.com>
2024-04-16 13:55:17 +0900

    Add files via upload

---
+++ js/big_hand_srevice.js
@@ -0,0 +1,39 @@
+const tab = document.querySelectorAll('.tab div');
+const tab_content = document.querySelectorAll('section');
+const accordion = document.querySelectorAll('.accordion');
+const accordionPerant = document.querySelector('ul');
+
+
+accordion.forEach(function(e){
+    e.addEventListener('click', function(e){
+        if(e.currentTarget.classList.contains('accordion_on')){
+            e.currentTarget.classList.remove('accordion_on');
+        }else{
+            accordion.forEach(function(e){
+                e.classList.remove('accordion_on');
+            });
+            e.currentTarget.classList.add('accordion_on');
+        }
+    });
+})
+
+
+
+
+
+
+
+
+tab[0].addEventListener('click',function(){
+    tab[1].classList.remove('tab_on');
+    tab[0].classList.add('tab_on');
+    tab_content[1].style.display = 'none';
+    tab_content[0].style.display = 'block';
+});
+
+tab[1].addEventListener('click',function(){
+    tab[0].classList.remove('tab_on');
+    tab[1].classList.add('tab_on');
+    tab_content[0].style.display = 'none';
+    tab_content[1].style.display = 'block';
+});
